68a4fd960823624db09caba9fdaab2707edc9ab2
Your Namedantakelly <you@example.com>
2024-06-30 13:16:06 -0500

    20th commit

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,6 @@
-// Example next.config.js (ES module)
-export default {
-    reactStrictMode: true, // Ensures strict mode in React
-    // Additional configurations if necessary
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
   
