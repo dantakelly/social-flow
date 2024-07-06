cf4e161c93449f7ca5931b14b3ecfc9bafd9fba7
Your Namedantakelly <you@example.com>
2024-07-06 18:07:19 -0500

    39th commit

--- client/src/app/authentication/login/page.jsx
+++ client/src/app/authentication/login/page.jsx
@@ -30,7 +30,7 @@ function Login() {
 
 
       try { 
-        const response = await fetch('http://localhost:3001/api/login', { 
+        const response = await fetch('https://social-flow-server.vercel.app/api/login', { 
           method: 'POST', 
           headers: { 
             'Content-Type': 'application/json', 
--- client/src/app/authentication/signup/page.jsx
+++ client/src/app/authentication/signup/page.jsx
@@ -32,7 +32,7 @@ function Signup() {
         }
 
         try { 
-            const response = await fetch('http://localhost:3001/api/user', { 
+            const response = await fetch('https://social-flow-server.vercel.app/api/user', { 
                 method: 'POST', 
                 headers: { 
                     'Content-Type': 'application/json',
